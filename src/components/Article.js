ce727d3d6966113e1ec194370d96832e2bb7a648
Matthew <turbodieseldodge@gmail.com>
2023-02-01 10:46:01 -0600

    mini project

---
+++ src/components/Article.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+function makeEmojiList(minutes) {
+    const interval = minutes < 30 ? 5 : 10
+    const emoji = minutes < 30 ? "☕️" : "🍱"
+
+    let emojis = ""
+    for (let index = 0; index < minutes; index += interval) {
+        emojis += emoji
+    }
+    return emojis
+}
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    const emojis = makeEmojiList(minutes)
+
+    return (
+        <Article>
+            <h3>{title}</h3>
+            <small>
+                {date} • {emojis} {minutes} min read
+            </small>
+            <p>{preview}</p>
+        </Article>
+    )
+}
+
+export default Article
+
